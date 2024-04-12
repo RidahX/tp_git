a6aa5a454a52abdf616ba975001a80750809bcbb
nicol <laconis9414@gmail.com>
2024-04-12 15:35:21 +0200

    Implement division function

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def subtraction(a, b):
+    return a - b
+    return result
+
+def addition(a, b):
+    result = a + b
+    return result
+
+def division(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
