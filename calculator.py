ba132f8c73cc2b50d5558304c66b0ad860968b63
nicol <laconis9414@gmail.com>
2024-04-12 16:00:02 +0200

    Add unit tests for calculator functions

--- calculator.py
+++ calculator.py
@@ -1,3 +1,11 @@
 def addition(a, b):
     result = a + b
     return result
+
+def soustraction(a, b):
+    return a - b
+
+def division(a, b):
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
